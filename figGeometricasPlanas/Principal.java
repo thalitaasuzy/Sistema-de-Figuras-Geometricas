ae1592e9ab82a87dc9217eb1a922b8bceb3a4921
Thalita Rodrigues <112733274+thalitaasuzy@users.noreply.github.com>
2023-04-28 13:55:46 -0300

    Create Principal.java

---
+++ figGeometricasPlanas/Principal.java
@@ -0,0 +1,35 @@
+package br.edu.principal;
+
+
+
+import br.edu.figGeometricas;
+
+
+
+public class Principal {
+
+
+
+	public static void main(String[] args) {
+
+	
+
+		Triangulo objT1 = new Triangulo();
+
+		
+
+		objT1.base = 3;
+
+		objT1.altura = 4;
+
+		
+
+		double result = calcArea1(); 
+
+
+
+	}
+
+
+
+}
