b68664936ad9d5a70d8f64ecc46b62ac9f2fca73
Thalita Rodrigues <112733274+thalitaasuzy@users.noreply.github.com>
2023-05-09 15:09:03 -0300

    Update Principal.java

--- Principal/Principal.java
+++ Principal/Principal.java
@@ -12,19 +12,11 @@ public class Principal {
 
 	public static void main(String[] args) {
 
-	
-
-		Triangulo objT1 = new Triangulo();
-
-		
-
-		objT1.base = 3;
-
-		objT1.altura = 4;
-
-		
-
-		double result = calcArea1(); 
+		//FIGURAS GEOMETRICAS ESPACIAIS
+		Piramide P1 = new Piramide();
+		P1.setBase(6);
+		P1.setAltura(5);
+		P1.listaAtributos();
 
 
 
