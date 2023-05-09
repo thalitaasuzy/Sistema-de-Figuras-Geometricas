91e21025bbc387ea1291489825896af51b6acb5c
Thalita Rodrigues <112733274+thalitaasuzy@users.noreply.github.com>
2023-05-09 15:07:57 -0300

    Update Piramide.java

--- figGeometricasEspaciais/Piramide.java
+++ figGeometricasEspaciais/Piramide.java
@@ -1,87 +1,57 @@
 package br.edu.figuGeometricasEspaciais;
 
-import java.lang.Math.*;
 public class Piramide {
-
-	//Atributos
-    private double apotemaPiramide;
-    private double apotemaBase;
-    private double arestaBase;
-    private double areaFace;
-    private double areaBase;
-    private double areaLateral;
-    private double areaTotal;
-    private double volume;
+    private double base;
     private double altura;
-    private double alturaPiramide;
     
-//Metódos
-
-    public void calcAreaBase() {
-        this.areaBase = (arestaBase*arestaBase);
+    // Método getter para obter o valor da base da pirâmide
+    public double getBase() {
+        return base;
     }
     
-    public void calcAreaFace() {
-        this.areaFace = (arestaBase * apotemaPiramide /2);
+    // Método setter para definir o valor da base da pirâmide
+    public void setBase(double base) {
+        this.base = base;
     }
     
-    public void calcAreaLateral() {
-        this.areaLateral = 3 * areaFace;
-    }
-    
-    public void calcAreaTotal() {
-        
-        this.areaTotal = areaLateral + areaBase;
-    }
-    
-    public void calcApotemaBase() {
-        this.apotemaBase = (arestaBase/2);
-    }
-    
-    public void calcApotemaPiramide() {
-        
-        this.apotemaPiramide = math.sqrt((alturaPiramide*alturaPiramide) * (apotemaBase*apotemaBase)); 
-    }
-    
-    public void calcVolume() {
-        
-        this.volume = (areaBase * alturaPiramide / 3); 
-    }
-    
-    public double getArestaBase() {
-        return this.arestaBase;
-    }
-    
-    public void setArestaBase(double AB){
-        this.arestaBase = AB;
-    }
-    
+    // Método getter para obter o valor da altura da pirâmide
     public double getAltura() {
-        return this.altura;
+        return altura;
     }
     
-    public void setAltura(double H){
-        this.altura = H;
+    // Método setter para definir o valor da altura da pirâmide
+    public void setAltura(double altura) {
+        this.altura = altura;
     }
     
+    // Método para calcular e retornar a área da base da pirâmide
+    public double calcularAreaBase() {
+        return base * base;
+    }
     
+    // Método para calcular e retornar a área lateral da pirâmide
+    public double calcularAreaLateral() {
+        return (base * altura / 2);
+    }
     
+    // Método para calcular e retornar o volume da pirâmide
+    public double calcularVolume() {
+        return (base * base * altura) / 3;
+    }
     
-    public void listarAtributos() {
-        
-        System.out.println("===========PIRAMIDE QUADRANGULAR===========");
-        
-        System.out.println("O valor do lado é " + getArestaBase() + "cm.");
-        
-        System.out.println("O valor da área de cada face é " + areaFace + "cm².");
+    // Método para imprimir as características da pirâmide
+    public void listaAtributos() {
+    
+    	    System.out.println("===========PIRÂMIDE QUADRANGULAR===========");
+    	    
+    		System.out.println("O valor do base é " + getBase() + "cm.");
+    		
+    		System.out.println("O valor do altura é " + getAltura() + "cm.");
 
-        System.out.println("O valor da área lateral é " + areaLateral + "cm².");
-		
-        System.out.println("O valor da área base é " + areaBase + "cm².");
-		
-        System.out.println("O valor da área total é " + areaTotal + "cm².");
-		
-        System.out.println("O valor do volume é: " + volume + "cm³.");
+    		System.out.println("O valor da área da base é  " +  calcularAreaBase() + "cm².");
+    		
+    		System.out.println("O valor da área lateral é  " +  calcularAreaLateral() + "cm².");
+    		
+    		System.out.println("O valor do volume é " + calcularVolume() + "cm³");
     }
-    
 }
